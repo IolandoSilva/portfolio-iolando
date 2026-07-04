3c96380d05cb4faf2668d6b3dc61f727267ebf9c
IolandoSilva <www.iolando12silva54@gmail.com>
2026-07-04 17:31:14 -0300

    Delete Scripts.js

--- Scripts.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function () {
-  const cabecalho = document.querySelector(".cabecalho");
-
-  if (window.scrollY > 80) {
-    cabecalho.classList.add("scroll");
-  } else {
-    cabecalho.classList.remove("scroll");
-  }
-});
